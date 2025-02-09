00f770f88848b2518bb239bb3785b7ebf5c9e9dc
I_Run_For_Fun <bgoo620@aucklanduni.ac.nz>
2025-02-09 22:35:51 +1300

    Created envelope

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".valentines").addEventListener("click", function() {
+  // Toggle the 'open' class to trigger CSS transitions
+  document.querySelector(".envelope").classList.toggle("open");
+});
