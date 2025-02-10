fdecc6dc66b99027ae657b6efb67a2ce45937998
bgoo620 <113594416+bgoo620@users.noreply.github.com>
2025-02-10 23:18:50 +1300

    Text and options added

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.querySelector(".valentines").addEventListener("click", function() {
   // Toggle the 'open' class to trigger CSS transitions
   document.querySelector(".envelope").classList.toggle("open");
+  document.getElementById("title").innerText = "";
+  document.getElementById("title").innerHTML = "<div id=\"buttons\"><button class=\"options\" id=\"yes\">Yes</button><button class=\"options\" id=\"no\">No</button></div>"
+  document.getElementById("buttons").classList.add("show");
+  // document.getElementById("title").innerText = "changed";
 });
